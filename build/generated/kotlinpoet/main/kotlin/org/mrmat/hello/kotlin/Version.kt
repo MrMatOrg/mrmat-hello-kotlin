7a87cdd05a8f4c8d01e061c43613da7bcc16bbac
Mathieu Imfeld <imfeldma@gmail.com>
2022-11-05 15:23:22 +0100

    Cleaned up from missing .gitignore

--- build/generated/kotlinpoet/main/kotlin/org/mrmat/hello/kotlin/Version.kt
+++
@@ -1,9 +0,0 @@
-package org.mrmat.hello.kotlin
-
-import kotlin.String
-
-public class Version {
-  public companion object {
-    public val VERSION: String = "0.0.0-SNAPSHOT"
-  }
-}
--- build/org/mrmat/hello/kotlin/Version.kt
+++
@@ -1,7 +0,0 @@
-package org.mrmat.hello.kotlin
-
-import kotlin.String
-
-public class Version {
-  public val VERSION: String = "0.0.0-SNAPSHOT"
-}

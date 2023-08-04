1c66dbf7292620ca360b78058304a75b5ef35a6d
Mathieu Imfeld <imfeldma@gmail.com>
2023-08-04 22:40:19 +0200

    Updated dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -32,8 +32,8 @@ dependencies {
 
     //
     // For parsing YAML
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 }
 
 gradlePlugin {

5f2aa95bc41498b917e6d21542c51acb2fb0781f
Mathieu Imfeld <imfeldma@gmail.com>
2024-04-29 19:43:28 +0200

    Updated dependencies

--- apps/versioning/src/test/kotlin/org/mrmat/hello/kotlin/test/VersionTest.kt
+++ apps/versioning/src/test/kotlin/org/mrmat/hello/kotlin/test/VersionTest.kt
@@ -4,7 +4,6 @@ import org.mrmat.hello.kotlin.app.versioning.Version
 import kotlin.test.Test
 
 class VersionTest {
-
     @Test
     fun checkVersion() {
         val envVersion = System.getenv("MRMAT_VERSION") ?: "0.0.0-SNAPSHOT"
